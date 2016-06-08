f31eb1a752283951c58e9d2ab346703449fdb1c9
leandrochl <leandrochl@outlook.com>
2016-06-07 21:58:04 -0300

    Add files via upload

--- src/java/br/com/egames/ProdutoDAO.java
+++ src/java/br/com/egames/ProdutoDAO.java
@@ -5,180 +5,161 @@
  */
 package br.com.egames;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author Leandro
  */
 public class ProdutoDAO {
+
     //Método para inserção de dados
-    public String inserir(Produto produto){
+
+    public String inserir(Produto produto) {
         String sql = "insert into produto ";
         sql += "values (?,?,?)";
         Connection con = Conexao.abrirConexao();
-        try{
+        try {
             PreparedStatement pst = con.prepareStatement(sql);
-            
+
             pst.setInt(1, produto.getQtdeEstoque());
             pst.setString(2, produto.getDescricao());
             pst.setDouble(3, produto.getPreco());
             pst.setInt(4, produto.getIdProduto());
             pst.setString(5, produto.getDataCadastro());
-            
-            if(pst.executeUpdate() > 0){
+
+            if (pst.executeUpdate() > 0) {
                 Conexao.fecharConexao(con);
                 return "Registro inserido com sucesso.";
-            }else{
+            } else {
                 Conexao.fecharConexao(con);
                 return "Erro ao inserir registro.";
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             Conexao.fecharConexao(con);
             return e.getMessage();
-        }        
+        }
     }
-    
+
     //Método para alteração de dados
-    public String alterar(Produto produto){
+    public String alterar(Produto produto) {
         String sql = "update produto set ";
         sql += "qtdeestoque=?,"
-             + "descricao=?,"
-             + "preco=?,"
-             + "datacadastro=?";
+                + "descricao=?,"
+                + "preco=?,"
+                + "datacadastro=?";
         sql += " where idproduto=?";
         Connection con = Conexao.abrirConexao();
-        try{
+        try {
             PreparedStatement pst = con.prepareStatement(sql);
-            
+
             pst.setInt(1, produto.getQtdeEstoque());
             pst.setString(2, produto.getDescricao());
             pst.setDouble(3, produto.getPreco());
             pst.setInt(4, produto.getIdProduto());
             pst.setString(5, produto.getDataCadastro());
-            
-            if(pst.executeUpdate() > 0){
+
+            if (pst.executeUpdate() > 0) {
                 Conexao.fecharConexao(con);
                 return "Registro alterado com sucesso.";
-            }else{
+            } else {
                 Conexao.fecharConexao(con);
                 return "Erro ao alterar registro.";
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             Conexao.fecharConexao(con);
             return e.getMessage();
         }
     }
+
     //Método para excluir dados
-    public String excluir(Cargo cargo){
-        String sql = "delete from cargo ";
-        sql += "where codigo=?";
+
+    public String excluir(Produto produto) {
+        String sql = "delete from produto ";
+        sql += "where idproduto=?";
         Connection con = Conexao.abrirConexao();
-        try{
-            PreparedStatement pst =
-                    con.prepareStatement(sql);
-            pst.setInt(1, cargo.getCodigo());
-            if(pst.executeUpdate() > 0){
+        try {
+            PreparedStatement pst = con.prepareStatement(sql);
+
+            pst.setInt(1, produto.getIdProduto());
+
+            if (pst.executeUpdate() > 0) {
                 Conexao.fecharConexao(con);
                 return "Registro excluído com sucesso.";
-            }else{
+            } else {
                 Conexao.fecharConexao(con);
                 return "Erro ao excluir registro.";
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             Conexao.fecharConexao(con);
             return e.getMessage();
         }
     }
-    
+
     //Método para listar todos os cargos
-    public List<Cargo> listarTodos(){
-        String sql = "select * from cargo";
+    public List<Produto> listarTodos() {
+        String sql = "select * from produto";
         Connection con = Conexao.abrirConexao();
-        List<Cargo> lista = new ArrayList<>();
-        try{
-           PreparedStatement pst = 
-                   con.prepareStatement(sql);
+        List<Produto> lista = new ArrayList<>();
+        try {
+            PreparedStatement pst = con.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
-            if(rs != null){
-                while(rs.next()){
-                    Cargo c = new Cargo();
-                    c.setCodigo(rs.getInt(1));
+            if (rs != null) {
+                while (rs.next()) {
+                    Produto c = new Produto();
+
+                    c.setQtdeEstoque(rs.getInt(1));
                     c.setDescricao(rs.getString(2));
-                    c.setSalario(rs.getDouble(3));
+                    c.setPreco(rs.getDouble(3));
+                    c.setIdProduto(rs.getInt(4));
+                    c.setDataCadastro(rs.getString(5));
+
                     lista.add(c);
                 }
                 Conexao.fecharConexao(con);
                 return lista;
-            }else{
+            } else {
                 Conexao.fecharConexao(con);
                 return null;
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             Conexao.fecharConexao(con);
             return null;
         }
     }
-    
+
     //Método que retorna um objeto, de acordo
     //com o código
-    public Cargo buscaPorCodigo(int cod){
-        String sql = "select * from cargo ";
-        sql += "where codigo=?";
+    public Produto buscaPorCodigo(int cod) {
+        String sql = "select * from produto ";
+        sql += "where idproduto=?";
         Connection con = Conexao.abrirConexao();
-        try{
+        try {
             PreparedStatement pst = con.prepareStatement(sql);
             pst.setInt(1, cod);
             ResultSet rs = pst.executeQuery();
-            if(rs.next()){
-                Cargo c = new Cargo();
-                c.setCodigo(rs.getInt(1));
+            if (rs.next()) {
+                Produto c = new Produto();
+                c.setQtdeEstoque(rs.getInt(1));
                 c.setDescricao(rs.getString(2));
-                c.setSalario(rs.getDouble(3));
+                c.setPreco(rs.getDouble(3));
+                c.setIdProduto(rs.getInt(4));
+                c.setDataCadastro(rs.getString(5));
                 Conexao.fecharConexao(con);
                 return c;
-            }else{
+            } else {
                 Conexao.fecharConexao(con);
-                
+
                 return null;
             }
-        }catch(SQLException e){
-            Conexao.fecharConexao(con);            
-            return null;
-        }
-    }
-    
-    //Método que retorna um objeto, de acordo
-    //com a descrição
-    public Cargo buscaPorDescricao(String descri){
-        String sql = "select * from cargo ";
-        sql += "where descricao=?";
-        Connection con = Conexao.abrirConexao();
-        try{
-            PreparedStatement pst =
-                    con.prepareStatement(sql);
-            pst.setString(1, descri);
-            ResultSet rs = pst.executeQuery();
-            if(rs.next()){
-                Cargo c = new Cargo();
-                c.setCodigo(rs.getInt(1));
-                c.setDescricao(rs.getString(2));
-                c.setSalario(rs.getDouble(3));
-                Conexao.fecharConexao(con);
-                return c;
-            }else{
-                Conexao.fecharConexao(con);
-                return null;
-            }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             Conexao.fecharConexao(con);
             return null;
         }
     }
 }
-
--- src/java/br/com/egames/Transportadora.java
+++ src/java/br/com/egames/Transportadora.java
@@ -10,5 +10,76 @@ package br.com.egames;
  * @author Leandro
  */
 public class Transportadora {
-    
+    private String cnpjTransp;
+    private String enderecoTransp;
+    private String nomeTransp;
+    private String emailTransp;
+    private Integer cpfCliente;
+    private String idNota;
+    private String idPedido;
+    private String idTelefone;
+
+    public String getCnpjTransp() {
+        return cnpjTransp;
+    }
+
+    public void setCnpjTransp(String cnpjTransp) {
+        this.cnpjTransp = cnpjTransp;
+    }
+
+    public String getEnderecoTransp() {
+        return enderecoTransp;
+    }
+
+    public void setEnderecoTransp(String enderecoTransp) {
+        this.enderecoTransp = enderecoTransp;
+    }
+
+    public String getNomeTransp() {
+        return nomeTransp;
+    }
+
+    public void setNomeTransp(String nomeTransp) {
+        this.nomeTransp = nomeTransp;
+    }
+
+    public String getEmailTransp() {
+        return emailTransp;
+    }
+
+    public void setEmailTransp(String emailTransp) {
+        this.emailTransp = emailTransp;
+    }
+
+    public Integer getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(Integer cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public String getIdNota() {
+        return idNota;
+    }
+
+    public void setIdNota(String idNota) {
+        this.idNota = idNota;
+    }
+
+    public String getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(String idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getIdTelefone() {
+        return idTelefone;
+    }
+
+    public void setIdTelefone(String idTelefone) {
+        this.idTelefone = idTelefone;
+    }
 }
